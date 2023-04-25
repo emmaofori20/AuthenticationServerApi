2720e2f4f567bdcded344254714730085c834078
Emmanuel Ofori <63444813+emmaofori20@users.noreply.github.com>
2023-04-25 09:43:15 +0000

    Add files via upload

---
+++ shared/DTOs/LoginDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared.DTOs
+{
+	public class LoginDTO
+	{
+		[Required(ErrorMessage = "User Name is required")]
+		public string? Username { get; set; }
+
+		[Required(ErrorMessage = "Password is required")]
+		public string? Password { get; set; }
+	}
+
+	public class LoginUserDTO
+	{
+		public string Username { get; set; }
+		public DateTime expirationDate { get; set; }
+		public string Token { get; set; }
+		public string Id { get; set; }
+		public List<string> roles { get; set; }
+	}
+
+	public class AuthenticateModel
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public int ApplicationId { get; set; }
+	}
+}
